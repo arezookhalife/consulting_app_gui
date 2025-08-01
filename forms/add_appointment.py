58aeed1007a2890686c44cd5459afe3e606038e5
arezoo <arezoo.kh1988@gmail.com>
2025-08-01 18:18:40 +0330

    Add date & time validation to add_appointment madule.

--- forms/add_appointment.py
+++ forms/add_appointment.py
@@ -1,39 +1,51 @@
 import tkinter as tk
 from tkinter import messagebox
+from datetime import datetime
 import json
 
 def add_appointment():
     def save_appointment():
         consultant_id = consultant_var.get()
-        date = entry_date.get()
-        time = entry_time.get()
+        date_input = entry_date.get()
+        time_input = entry_time.get()
 
-        if not consultant_id or not date or not time:
+
+        if not consultant_id or not date_input or not time_input:
             messagebox.showerror("خطا", "تمام فیلدها باید پر شوند!")
             return
-        
-        try:
-            with open("data/appointments.json", "r") as file:
-                appointments = json.load(file)
-        except FileNotFoundError:
-            appointments = []
+        else:
+            try:
+                input_date = datetime.strptime(date_input, "%Y-%m-%d").date()
+                input_time = datetime.strptime(time_input,"%H:%M").time()
+                input_datetime = datetime.combine(input_date, input_time)
+                if input_datetime > datetime.now():
+                    try:
+                        with open("data/appointments.json", "r") as file:
+                            appointments = json.load(file)
+                    except FileNotFoundError:
+                        appointments = []
             
             
-        new_id= appointments[-1]["id"]+ 1 if appointments else 1
-        new_appointment = {
-            "id": new_id,
-            "consultant_id": int(consultant_id),
-            "date": date,
-            "time": time
-        }
+                    new_id= appointments[-1]["id"]+ 1 if appointments else 1
+                    new_appointment = {
+                        "id": new_id,
+                        "consultant_id": int(consultant_id),
+                        "date": date_input,
+                        "time": time_input
+                    }
 
-        appointments.append(new_appointment)
+                    appointments.append(new_appointment)
 
-        with open("data/appointments.json", "w") as file:
-            json.dump(appointments, file, indent=4)
+                    with open("data/appointments.json", "w") as file:
+                        json.dump(appointments, file, indent=4)
 
-        messagebox.showinfo("موفقیت", "نوبت با موفقیت ثبت شد!")
-
+                    messagebox.showinfo("موفقیت", "نوبت با موفقیت ثبت شد!")
+                else:
+                    messagebox.showerror("خطا", "امکان ثبت نوبت در تاریخ یا ساعت وارد شده وجود ندارد!")
+            except ValueError:
+                messagebox.showerror("خطا", "فرمت تاریخ یا ساعت وارد شده صحیح نیست!")
+                
+            
     root = tk.Tk()
     root.title("ثبت نوبت")
     root.geometry("400x300")
